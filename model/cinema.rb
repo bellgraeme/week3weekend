6da5a6a870d4898eefe0ce303258ce73e32c3008
Graeme Bell <bell.graeme@gmail.com>
2017-02-19 16:27:16 +0000

    cracked it in one huge messy method :smile:

--- model/cinema.rb
+++ model/cinema.rb
@@ -1,19 +1,96 @@
-class Cinema
+require_relative ('ticket.rb')
+require_relative ('film.rb')
+require_relative ('customer.rb')
+require 'pry'
 
-  attr_reader :name, :funds
+class Cinema 
 
-  def initialize (name, funds)
+  attr_accessor :name, :funds
+
+  def initialize (name)
     @name = name
-    @funds = funds.to_i
+  
+  
   end
 
-  def sell_ticket(customer, film)
-    customer.funds = customer.funds - film.price
-    customer.update()
-    @funds += film.price
-    ticket_sold = Ticket.new({'customer_id' => customer.id, 'film_id' => film.id ,'show' => '23:00'})
-    ticket_sold.save()
-    return ticket_sold
+
+  def self.setup
+    @film1 = Film.new('title' => 'Dances with Wombles', 'price' => 4)
+    @film1.save()
+    @film2 = Film.new('title' => 'Mission Impossibru', 'price' => 5)
+    @film2.save()
+    @film3 = Film.new('title' => 'Jaws 18, The Final Final Final Bite Again!', 'price' => 6)
+    @film3.save()
+    @film4 = Film.new('title' => 'Star Trekking through the Universe', 'price' => 7)
+    @film4.save()
   end
 
+
+  @funds = 0
+
+  @screen1 = 0
+  @screen2 = 0
+  @screen3 = 0
+  @screen4 = 0
+  
+
+
+  def self.sell_ticket(customer)
+    self.setup
+    puts "Would you like to see: \n 1. Dances with Wombles? \n 2. Mission Impossibru? \n 3. Jaws 18, The Final Final Final Bite Again? \n 4. Star Trekking through the Universe?"
+    answer = gets.chomp.to_i
+    case answer
+    when 1
+      if @screen1 < 40
+        @screen1 += 1
+        # binding.pry
+        customer.funds = customer.funds - @film1.price
+        customer.update()
+        @funds += @film1.price
+        ticket_sold = Ticket.new({'customer_id' => customer.id, 'film_id' => @film1.id ,'show' => Time.now})
+        ticket = ticket_sold.save()
+        puts "Good choice, I see you are  a person of refined taste"
+        return ticket
+      else puts "Sorry comupter says: No"
+      end
+    when 2
+      if  @screen1 < 40
+        @screen1 += 1
+        customer.funds = customer.funds - @film2.price
+        customer.update()
+        @funds += @film2.price
+        ticket_sold = Ticket.new({'customer_id' => customer.id, 'film_id' => @film2.id ,'show' => Time.now})
+        ticket_sold.save()
+        puts "Good choice, I see you are  a person of refined taste"
+        return ticket_sold
+      else puts "Sorry comupter says: No"
+      end
+
+    when 3
+      if  @screen3 < 40
+        @screen3 += 1
+        customer.funds = customer.funds - @film3.price
+        customer.update()
+        @funds += @film3.price
+        ticket_sold = Ticket.new({'customer_id' => customer.id, 'film_id' => @film3.id ,'show' => Time.now})
+        ticket_sold.save()
+        puts "Good choice, I see you are  a person of refined taste"
+        return ticket_sold
+      else puts "Sorry comupter says: No"
+      end
+    when 4
+      if  @screen4 < 40
+        @screen4 += 1
+        customer.funds = customer.funds - @film4.price
+        customer.update()
+        @funds += @film4.price
+        ticket_sold = Ticket.new({'customer_id' => customer.id, 'film_id' => @film4.id ,'show' => Time.now})
+        ticket_sold.save()
+        puts "Good choice, I see you are a person of refined taste"
+        return ticket_sold
+      else puts "Sorry comupter says: No"
+      end
+    end
+  end
 end
+
--- model/console.rb
+++ model/console.rb
@@ -8,7 +8,7 @@ Customer.delete_all
 Film.delete_all
 Ticket.delete_all
 
-cinema = Cinema.new('cinema', 0)
+cinema = Cinema.new('cinema')
 
 customer1 = Customer.new('name' => 'Garth', 'funds' => 20)
 customer1.save()
@@ -22,31 +22,34 @@ customer3.save()
 customer4 = Customer.new('name' => 'Sarah', 'funds' => 35)
 customer4.save()
 
-film1 = Film.new('title' => 'Dances with Wombles', 'price' => 4)
-film1.save()
+# film1 = Film.new('title' => 'Dances with Wombles', 'price' => 4)
+# film1.save()
 
-film2 = Film.new('title' => 'Mission Impossibru', 'price' => 5)
-film2.save()
+# film2 = Film.new('title' => 'Mission Impossibru', 'price' => 5)
+# film2.save()
 
-film3 = Film.new('title' => 'Jaws 25, The final final final bite!', 'price' => 6)
-film3.save()
+# film3 = Film.new('title' => 'Jaws 18, The Final Final Final Bite Again!', 'price' => 6)
+# film3.save()
 
-film4 = Film.new('title' => 'Star Trekking through the Universe', 'price' => 7)
-film4.save()
+# film4 = Film.new('title' => 'Star Trekking through the Universe', 'price' => 7)
+# film4.save()
 
-ticket1 = Ticket.new('customer_id' => customer1.id, 'film_id' => film1.id, 'show' => '13:00')
-ticket1.save()
+# ticket1 = Ticket.new('customer_id' => customer1.id, 'film_id' => film1.id, 'show' => '13:00')
+# ticket1.save()
 
-ticket2 = Ticket.new('customer_id' => customer2.id, 'film_id' => film2.id, 'show' => '14:00')
-ticket2.save()
+# ticket2 = Ticket.new('customer_id' => customer2.id, 'film_id' => film2.id, 'show' => '14:00')
+# ticket2.save()
 
-ticket3 = Ticket.new('customer_id' => customer3.id, 'film_id' => film3.id, 'show' => '15:00')
-ticket3.save()
+# ticket3 = Ticket.new('customer_id' => customer3.id, 'film_id' => film3.id, 'show' => '15:00')
+# ticket3.save()
 
-ticket4 = Ticket.new('customer_id' => customer4.id, 'film_id' => film4.id, 'show' => '16:00')
-ticket4.save()
+# ticket4 = Ticket.new('customer_id' => customer4.id, 'film_id' => film4.id, 'show' => '16:00')
+# ticket4.save()
 
-ticket5 = cinema.sell_ticket(customer4, film2)
+Cinema.sell_ticket(customer4)
+Cinema.sell_ticket(customer2)
+Cinema.sell_ticket(customer3)
+Cinema.sell_ticket(customer1)
 
 
 binding.pry
